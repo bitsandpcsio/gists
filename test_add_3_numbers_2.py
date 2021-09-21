7c4c04e2c28d8d4d843e38c8b290fedf38ea953b
bitsandpcsio <info@bitsandpcs.io>
2021-09-21 16:00:14 +0100

    master: NEw tests

---
+++ test_add_3_numbers_2.py
@@ -0,0 +1,12 @@
+import pytest
+from add_3_numbers import add_3_numbers
+
+
+def test_add_3_numbers_1():
+    assert add_3_numbers(1, 1, 1) == 3
+
+def test_add_3_numbers_2():
+    assert add_3_numbers(7, 1, 3) == 11
+
+def test_add_3_numbers_3():
+    assert add_3_numbers(45, 897, 999) == 1941
